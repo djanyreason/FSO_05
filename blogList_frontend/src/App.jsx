87536153d7a10a64cde2e49bde67a1e4127cbe77
djanyreason <djanyreason@gmail.com>
2023-09-11 10:07:51 -0400

    Completes ex. 5.2, saves login to local storage
    This commit saves login to local storage, so that when a user logs in
    this remains after the page is reloaded.
    
    This commit also adds a logout button which is rendered in the
    Bloglist component next to the user's name. Logout functionality is
    handled in a doLogout function which is defined in app.jsx and passed
    to the Bloglist component.

--- blogList_frontend/src/App.jsx
+++ blogList_frontend/src/App.jsx
@@ -14,11 +14,21 @@ const App = () => {
     );
   }, []);
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBloglistUser');
+    if(loggedUserJSON) {
+      const loggedUser = JSON.parse(loggedUserJSON);
+      setUser(loggedUser);
+      blogService.setToken(loggedUser.token);
+    }
+  }, []);
+
   const doLogin = async (credentials) => {
     if(user) return null;
 
     try {
       const userLogin = await loginService.login(credentials);
+      window.localStorage.setItem('loggedBloglistUser', JSON.stringify(userLogin));
       blogService.setToken(userLogin.token);
       setUser(userLogin);
     } catch (exception) {
@@ -26,11 +36,17 @@ const App = () => {
     }
   };
 
+  const doLogout = () => {
+    window.localStorage.removeItem('loggedBloglistUser');
+    blogService.setToken(null);
+    setUser(null);
+  };
+
   return (
     <div>
       {user === null
         ? <Login doLogin={doLogin} />
-        : <Bloglist blogs={blogs} user={user}/>
+        : <Bloglist blogs={blogs} user={user} doLogout={doLogout}/>
       }
     </div>
   );
--- blogList_frontend/src/components/Bloglist.jsx
+++ blogList_frontend/src/components/Bloglist.jsx
@@ -1,12 +1,12 @@
 import Blog from './Blog';
 
-const Bloglist = ({ blogs, user }) => {
+const Bloglist = ({ blogs, user, doLogout }) => {
   if(!user) return (<div></div>);
 
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>{user.name} logged in<button onClick={doLogout}>logout</button></p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
