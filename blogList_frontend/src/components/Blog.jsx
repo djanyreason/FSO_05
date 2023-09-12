f66fe779282182d94957a146807bb37e65078eec
djanyreason <djanyreason@gmail.com>
2023-09-12 15:40:29 -0400

    Completes ex. 5.7, adds view/hide more blog info
    NB - exercise 5.6 was unknowingly completed prior to exercise 5.4
    
    This commit adds a button to each blog entry which allows the viewing
    or hiding of additional blog info (url, likes, user who added the
    blog to the bloglist). It also adds a border around the blog. Due to
    the intended presentation/layout of the additions, this is not done
    using the Togglable component, but is done with state variables in
    the Blog component (this is specified in the exercise as the intended
    way to complete it).

--- blogList_frontend/src/components/Blog.jsx
+++ blogList_frontend/src/components/Blog.jsx
@@ -1,7 +1,41 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useState } from 'react';
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  };
+
+  console.log(blog);
+
+  const [hidden, setHidden] = useState('true');
+  const toggleHidden = () => setHidden(!hidden);
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={toggleHidden} style={{ marginLeft: 10 }}>
+        { hidden
+          ? 'view'
+          : 'hide'
+        }
+      </button>
+      { hidden
+        ? <></>
+        : <div>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes}
+            <button onClick={() => null}>like</button>
+          </div>
+          <div>{blog.user.name}</div>
+        </div>
+      }
+    </div>
+  );
+};
 
 export default Blog;
